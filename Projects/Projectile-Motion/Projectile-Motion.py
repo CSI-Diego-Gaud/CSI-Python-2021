efac949421ff0f9c06077763936fcbf385201ec1
CSI-Diego-Gaud <25-019@sanignacio.pr>
2021-12-07 13:23:42 -0400

    Json

--- Projects/Projectile-Motion/Projectile-Motion.py
+++ Projects/Projectile-Motion/Projectile-Motion.py
@@ -2,17 +2,29 @@ import math
 import os
 import pathlib
 import json
-Gun = ["HK416A5"]
-Cartridge = ["5.56x45mm NATO"]
-Ammo = ["5.56x45mm Warmageddon"]
+from ExperimentalData import Pylance
+Gun = "HK416A5"
+Cartridge = "5.56x45mm NATO"
+Ammo = "5.56x45mm Warmageddon"
 ProjectileDistance = 910
-Building = ["Tour Eiffel"]
+Building = "Tour Eiffel"
 BuildingHeight = 324
 Gravity = -9.8
 print("Calculate")
-#t =math.sqrt(BuildingHeight)* 2/Gravity
-# def("Time")s
-#delta_X=math.sqrt(ProjectileDistance*t)
-print(f"In this experiment we would be testing shooting a spesific weapon from a the top of a building. We have the {Gun} with this {Cartridge} with this {Ammo} which has a distance of {ProjectileDistance} which will be shot from the top of the {Building} which is this tall {BuildingHeight}. All of this will show the result of these conditions if they were to take place in real life")
+t =math.sqrt(BuildingHeight)* 2/Gravity
+delta_X=math.sqrt(ProjectileDistance*t)
+print(f"In this experiment we would be testing shooting a spesific weapon from a the top of a building. We have the {Gun} with this {Cartridge} with this {Ammo} which has a distance of {ProjectileDistance} which will be shot from the top of the {Building} which is this tall {BuildingHeight}.{delta_X}is the displacement of the bullet from the origin point. {t} Is representative of time and thus how long it would take for the bullet to travel. All of this will show the result of these conditions if they were to take place in real life")
+myDataSet=""
+class ExperimentalData:
+    def __init__(self,Gun ,Cartridge ,Ammo ,ProjectileDistance ,Building ,BuildingHeight ,Gravity ):
+        self.Gun = Gun
+        self.Cartridge = Cartridge
+        self.Ammo = Ammo
+        self.ProjectileDistance = ProjectileDistance
+        self.Building = Building
+        self.BuildingHeight = BuildingHeight
+        self.Gravity = Gravity
 
-# def ProjectileFunction(
+
+
+# def ProjectileFunction 
